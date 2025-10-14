0456a0b13ea24fcaffc84462340008ad46297ba2
tanselbay1 <tanselbay1@hotmail.com>
2025-10-14 13:48:01 +0700

    Create newZombie function

---
+++ cpp01/ex00/newZombie.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 12:03:04 by tanselbayra       #+#    #+#             */
+/*   Updated: 2025/10/14 12:21:39 by tanselbayra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name){
+	Zombie* jim = new Zombie(name);
+	return jim;
+}
