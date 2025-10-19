e0e37054200167a5098029f461707d6fb8d80bc8
tanselbay1 <tanselbay1@hotmail.com>
2025-10-19 14:16:21 +0700

    Edit main.c corresponding to subject and edit Makefile for additional files

--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,16 +6,32 @@
 /*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 12:15:10 by tanselbayra       #+#    #+#             */
-/*   Updated: 2025/10/16 12:49:01 by tanselbayra      ###   ########.fr       */
+/*   Updated: 2025/10/19 14:03:27 by tanselbayra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "HumanA.hpp"
+#include "HumanB.hpp"
 #include "Weapon.hpp"
 
-int main(void){
-	Weapon machine_gun;
-	machine_gun.setType("Machine Gun");
-	std::cout << machine_gun.getType() << std::endl;
-	return (0);
+int main()
+{
+    {
+        Weapon club = Weapon("crude spiked club");
+		
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon club = Weapon("crude spiked club");
+		
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+    return 0;
 }
