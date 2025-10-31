28529f99bc9d814a82ec1700155305b30aee50ea
tanselbay1 <tanselbay1@hotmail.com>
2025-10-31 16:06:51 +0700

    Complete Fixed Class

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,16 +6,33 @@
 /*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 14:27:28 by tanselbayra       #+#    #+#             */
-/*   Updated: 2025/10/26 14:43:00 by tanselbayra      ###   ########.fr       */
+/*   Updated: 2025/10/31 15:46:39 by tanselbayra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
-Fixed::Fixed(void) {
-	std::cout << "Constructor called" << std::endl;
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed(void): _fixedPointValue(0) {
+	std::cout << "Default constructor called" << std::endl;
 	return;
+}
+
+Fixed::Fixed(const Fixed& src) {
+	std::cout << "Copy constructor called!" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	
+	if (this != &rhs)
+		this->_fixedPointValue = rhs.getRawBits();
+	
+	return *this;
 }
 
 Fixed::~Fixed(void) {
@@ -23,23 +40,12 @@ Fixed::~Fixed(void) {
 	return;
 }
 
-void Fixed::bar(int const i) const {
-	std::cout << "Member function bar with " << i << " runs!" << std::endl;
-	return;
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
 }
 
-void Fixed::bar(char const c) const {
-	std::cout << "Member function bar with " << c << " runs!" << std::endl;
-	return;
-}
-
-void Fixed::bar(float const f) const {
-	std::cout << "Member function bar with " << f << " runs!" << std::endl;
-	return;
-}
-
-void Fixed::bar(Fixed const & s) const {
-	(void)s;
-	std::cout << "Member function bar with class runs!" << std::endl;
-	return;
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedPointValue = raw;
 }
