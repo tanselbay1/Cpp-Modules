61e883be9601f4cc2d29ee0c07183033f1e597a0
tanselbay1 <tanselbay1@hotmail.com>
2025-10-31 15:16:14 +0700

    Complete Fixed Class Header

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,23 +6,30 @@
 /*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 14:22:48 by tanselbayra       #+#    #+#             */
-/*   Updated: 2025/10/26 15:29:55 by tanselbayra      ###   ########.fr       */
+/*   Updated: 2025/10/31 15:14:45 by tanselbayra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed {
+	
+private:
+	int _fixedPointValue;
+	int _fractionalBits;
 	
 public:
 	Fixed(void);
+	Fixed(const Fixed& src);
+	Fixed& operator=(const Fixed& rhs);
 	~Fixed(void);
 
-	void bar(int const i) const;
-	void bar(char const c) const;
-	void bar(float const f) const;
-	void bar(Fixed const & s) const;
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
 };
 
 #endif
