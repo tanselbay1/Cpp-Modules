95794552f9982acfb879756969dfeae407c8f239
tanselbay1 <tanselbay1@hotmail.com>
2025-11-10 13:36:22 +0700

    Initialize exercise with 'Comparison Operators'

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbay1 <tanselbay1@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 11:39:39 by tanselbay1        #+#    #+#             */
+/*   Updated: 2025/11/10 12:02:11 by tanselbay1       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+const int Fixed::_fractionalBits = 8;
+
+Fixed::Fixed(void): _fixedPointValue(0) {
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+// Constructor that takes an integer
+Fixed::Fixed(const int intValue) {
+	std::cout << "Int constructor called" << std::endl;
+	// Shift the integer left by 8 bits to make room for the fractional part
+	this->_fixedPointValue = intValue << _fractionalBits;
+}
+
+// Constructor that takes a float
+Fixed::Fixed(const float floatValue) {
+	std::cout << "Float constructor called" << std::endl;
+	// Multiply by 2^8 (256) and round to the nearest integer
+	this->_fixedPointValue = roundf(floatValue * (1 << _fractionalBits));
+}
+
+Fixed::Fixed(const Fixed& src) {
+	std::cout << "Copy constructor called!" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed& Fixed::operator=(const Fixed& rhs) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	
+	if (this != &rhs)
+		this->_fixedPointValue = rhs.getRawBits();
+	
+	return *this;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+int Fixed::getRawBits(void) const {
+	//std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fixedPointValue);
+}
+
+void Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fixedPointValue = raw;
+}
+
+// Convert the fixed-point value to a float
+float Fixed::toFloat(void) const {
+	// Divide by 2^8 (256) to get the float value back
+	return (float)this->_fixedPointValue / (float)(1 << _fractionalBits);
+}
+
+// Convert the fixed-point value to an integer
+int Fixed::toInt(void) const {
+	// Shift right by 8 bits to discard the fractional part
+	return this->_fixedPointValue >> _fractionalBits;
+}
+
+// Operator overload
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+	// Tell the stream to print the float representation of the Fixed object
+	os << fixed.toFloat();
+	return os;
+}
+
+// ----------- EX02 ----------
+
+// 1. Comparison Operators
+bool Fixed::operator>(const Fixed& rhs) const {
+    return (this->_fixedPointValue > rhs.getRawBits());
+}
+
+bool Fixed::operator<(const Fixed& rhs) const {
+    return (this->_fixedPointValue < rhs.getRawBits());
+}
+
+bool Fixed::operator>=(const Fixed& rhs) const {
+    return (this->_fixedPointValue >= rhs.getRawBits());
+}
+
+bool Fixed::operator<=(const Fixed& rhs) const {
+    return (this->_fixedPointValue <= rhs.getRawBits());
+}
+
+bool Fixed::operator==(const Fixed& rhs) const {
+    return (this->_fixedPointValue == rhs.getRawBits());
+}
+
+bool Fixed::operator!=(const Fixed& rhs) const {
+    return (this->_fixedPointValue != rhs.getRawBits());
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbay1 <tanselbay1@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 11:26:18 by tanselbay1        #+#    #+#             */
+/*   Updated: 2025/11/10 13:33:05 by tanselbay1       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+private:
+	int 				_fixedPointValue;
+	static const int 	_fractionalBits;
+
+public:
+	Fixed(void);
+	Fixed(const Fixed& src);
+	Fixed& operator=(const Fixed& src);
+	~Fixed(void);
+
+	// Constructors with values
+	Fixed(const int intValue);
+	Fixed(const float floatValue);
+
+	// Member functions
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+	// Getter - setter
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+    
+    // -------- EX02 -----------
+    
+    // 1. Comparison Operators
+    bool operator>(const Fixed& rhs) const;
+    bool operator<(const Fixed& rhs) const;
+    bool operator>=(const Fixed& rhs) const;
+    bool operator<=(const Fixed& rhs) const;
+    bool operator==(const Fixed& rhs) const;
+    bool operator!=(const Fixed& rhs) const;
+
+};
+
+// Printing Operator Overload - something like (a.print("hello"))
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
