73facd2cc18a50b32040079bfd6bc9197590e36b
tanselbay1 <tanselbay1@hotmail.com>
2025-11-11 16:12:05 +0700

    Implement ClapTrap Class .cpp file

--- cpp03/ex00/ClapTrap.hpp
+++ cpp03/ex00/ClapTrap.hpp
@@ -6,12 +6,12 @@
 /*   By: tanselbay1 <tanselbay1@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 12:16:47 by tanselbay1        #+#    #+#             */
-/*   Updated: 2025/11/11 12:41:58 by tanselbay1       ###   ########.fr       */
+/*   Updated: 2025/11/11 15:50:15 by tanselbay1       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAP_TRAP_HPP
-#define CLAP_TRAP_HPP
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
 #include <string>
 #include <iostream>
@@ -20,19 +20,20 @@ class ClapTrap {
     
 private:
     std::string     _name;
-    int             _hitPoints;
-    int             _energyPoints;
-    int             _attackDamage;
+    unsigned int    _hitPoints;
+    unsigned int    _energyPoints;
+    unsigned int    _attackDamage;
 
 public:
+    ClapTrap(void);
     ClapTrap(std::string name);
-    ClapTrap(const ClapTrap& src);
-    ClapTrap& operator=(const ClapTrap& rhs);
+    ClapTrap(const ClapTrap &src);
+    ClapTrap &operator=(const ClapTrap &rhs);
     ~ClapTrap(void);
 
     void attack(const std::string& target);
     void takeDamage(unsigned int amount);
-    void beRapaired(unsigned int amount);
+    void beRepaired(unsigned int amount);
 };
 
 #endif
