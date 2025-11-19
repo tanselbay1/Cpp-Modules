d3b7ae99d69404f9d1499b67a724709f00b6d809
tanselbay1 <tanselbay1@hotmail.com>
2025-11-19 14:24:39 +0700

    Implement Brain class .cpp file

---
+++ cpp04/ex01/Brain.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/19 14:18:39 by tanselbayra       #+#    #+#             */
+/*   Updated: 2025/11/19 14:22:59 by tanselbayra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Brain.hpp"
+
+Brain::Brain(void) {
+    std::cout << "🧠 Brain default constructor called." << std::endl;
+	
+    for (int i = 0; i < 100; ++i) {
+        this->ideas[i] = "Idea " + std::to_string(i); 
+    }
+}
+
+// DEEP COPY
+Brain::Brain(const Brain &src) {
+    std::cout << "🧠 Brain copy constructor called (Deep Copy)." << std::endl;
+    // 1. Copy the members (the array contents)
+    for (int i = 0; i < 100; ++i) {
+        this->ideas[i] = src.ideas[i];
+    }
+}
+
+Brain& Brain::operator=(const Brain &rhs) {
+    std::cout << "🧠 Brain copy assignment operator called (Deep Copy)." << std::endl;
+    if (this != &rhs) {
+        // 1. Deep copy the ideas array contents
+        for (int i = 0; i < 100; ++i) {
+            this->ideas[i] = rhs.ideas[i];
+        }
+    }
+    return *this;
+}
+
+Brain::~Brain(void) {
+    std::cout << "🧠 Brain destructor called." << std::endl;
+    // Nothing to manually delete here since the array is part of the object
+}
